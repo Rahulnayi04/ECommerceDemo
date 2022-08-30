4ac7d20857c59de2ba8648575f995a4b5c3caf54
Rahulnayi04 <112021214+Rahulnayi04@users.noreply.github.com>
2022-08-30 15:52:06 +0530

    Update Menu.js

--- src/components/MainHeader/Menu.js
+++ src/components/MainHeader/Menu.js
@@ -2,21 +2,17 @@ import react, { useState } from "react";
 import { ReactDOM } from "react";
 import "./Menu.css"
 import "font-awesome/css/font-awesome.min.css"
-const Menu = props => {
 
+const Menu = props => {
     const [showMakeup, setShowMakeup] = useState(false)
     const [showMakeupOptions, setshowMakeupOptions] = useState(false)
     const showMakeups = () => {
-
         if(showMakeup) {setShowMakeup(false)}
         else {setShowMakeup(true)}
-
         if(showMakeupOptions) {setshowMakeupOptions(false)}
-       
     }
     
     const showMakeupOptionsList = () => {
-
         if(showMakeupOptions) {setshowMakeupOptions(false)}
         else {setshowMakeupOptions(true)}
     }
@@ -25,7 +21,6 @@ const Menu = props => {
             <div className="overlay">
                 <div onClick={props.onMenuCloseClick} className="space"><span>Close menu <i className="fa fa-close"></i></span></div>
                 <div className="space"onClick={showMakeups}><span>Categories</span></div>
-                
                 {showMakeup && (
                     <div style={{cursor:'default', fontSize:'15px' }} >
                     <div className="space" onClick={showMakeupOptionsList}><li>Makeup</li></div>
@@ -43,8 +38,6 @@ const Menu = props => {
                     </div> 
                     )
                 }
-
-
             </div>
         )
 }
