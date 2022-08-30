d35a7c4577c5069576c106ab250cba8f219f6c9c
Rahulnayi04 <112021214+Rahulnayi04@users.noreply.github.com>
2022-08-30 15:50:46 +0530

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
-
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
-
 const Login = (props) => {
-  
   const [enteredEmail, setEnteredEmail] = useState('');
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
- 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
     // setEmailIsValid(enteredEmail.includes('@'));
