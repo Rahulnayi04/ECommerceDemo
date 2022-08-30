eb543e57894b178420d7ef46f00521933d62d749
Rahulnayi04 <112021214+Rahulnayi04@users.noreply.github.com>
2022-08-30 15:48:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+  const loginHandler = (email, password) => {    
     setIsLoggedIn(true);
   };
 
