68dbf7efaaa8a14e4815bef49d4953f60a90c30a
Rahulnayi04 <112021214+Rahulnayi04@users.noreply.github.com>
2022-08-30 15:23:59 +0530

    Update Menu.js

--- src/components/MainHeader/Menu.js
+++ src/components/MainHeader/Menu.js
@@ -1,13 +1,50 @@
-import react from "react";
+import react, { useState } from "react";
 import { ReactDOM } from "react";
 import "./Menu.css"
 import "font-awesome/css/font-awesome.min.css"
 const Menu = props => {
+
+    const [showMakeup, setShowMakeup] = useState(false)
+    const [showMakeupOptions, setshowMakeupOptions] = useState(false)
+    const showMakeups = () => {
+
+        if(showMakeup) {setShowMakeup(false)}
+        else {setShowMakeup(true)}
+
+        if(showMakeupOptions) {setshowMakeupOptions(false)}
+       
+    }
+    
+    const showMakeupOptionsList = () => {
+
+        if(showMakeupOptions) {setshowMakeupOptions(false)}
+        else {setshowMakeupOptions(true)}
+    }
+
     return (
             <div className="overlay">
-                <div onClick={props.onMenuCloseClick} className="space"><span>Close menu  <i className="fa fa-close"></i></span></div>
-                <div className="space"><span>Categories</span></div>
-                <div className="space"><span>Makeup</span></div>
+                <div onClick={props.onMenuCloseClick} className="space"><span>Close menu <i className="fa fa-close"></i></span></div>
+                <div className="space"onClick={showMakeups}><span>Categories</span></div>
+                
+                {showMakeup && (
+                    <div style={{cursor:'default', fontSize:'15px' }} >
+                    <div className="space" onClick={showMakeupOptionsList}><li>Makeup</li></div>
+                    </div> 
+                    )
+                }
+                {showMakeupOptions && (
+                    <div style={{cursor:'default', fontSize:'15px' }} >
+                    <ol>
+                    <div className="space"><li>Face Makeup</li></div>
+                    <div className="space"><li>Lip Makeup</li></div>
+                    <div className="space"><li>Eye Makeup</li></div>
+                    <div className="space"><li>Brushes and tools</li></div>
+                    </ol>
+                    </div> 
+                    )
+                }
+
+
             </div>
         )
 }
